8aff042567cdfedadf0bdc836eb3136eb73acad7
Harsh-Kumar-Barman <168334615+Harsh-Kumar-Barman@users.noreply.github.com>
2025-03-02 02:17:34 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'https://instagram-mern-clone-backend.onrender.com', // Your backend URL
+      '/api': 'http://localhost:5000', // Your backend URL
     },
   },
 });
