1bb94db3c04d0cd1a91a5ec7f50189483eb1962e
Harsh-Kumar-Barman <harshkumarbarman97@gmail.com>
2025-03-02 02:22:34 +0530

    update appjs origin *

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,8 @@ const MongoStore = require("connect-mongo");
 connectDB();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL
+  // origin: 'http://localhost:5173', // Replace with your frontend URL
+  origin: '*', // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT'], // Allowing GET, POST, and PUT
   credentials: true
 }));
