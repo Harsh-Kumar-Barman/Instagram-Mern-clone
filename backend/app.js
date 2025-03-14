714f37679c182a973754264a9a2954e4cd78f5f9
Harsh-Kumar-Barman <harshkumarbarman97@gmail.com>
2025-03-14 12:34:32 +0530

    update in appjs

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,6 @@ app.use('/api/story', storyRoutes);
 // Error handler
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
