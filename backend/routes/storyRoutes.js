4fcd7a3630d243410588ee54c7471acaa78917f5
Harsh-Kumar-Barman <harshkumarbarman97@gmail.com>
2025-03-14 14:02:13 +0530

    again add authmiddleware and update socketjs in backend

--- backend/routes/storyRoutes.js
+++ backend/routes/storyRoutes.js
@@ -2,10 +2,10 @@
 const express = require("express");
 const router = express.Router();
 const upload = require('../middlewares/uploadMiddleware');
-// const authMiddleware = require('../middlewares/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 const { storyUpload } = require("../controllers/storyController");
 
 // POST: Upload Story
-router.post("/uploadStory",  upload.single('media'), storyUpload);
+router.post("/uploadStory",authMiddleware,  upload.single('media'), storyUpload);
 
 module.exports = router;
