4d01bc4911926e9c58cb726e43341c89fe155f6e
Harsh-Kumar-Barman <harshkumarbarman97@gmail.com>
2025-03-14 19:25:37 +0530

    remove middeware from the baceknd

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/create', upload.array('media', 10), createPost); // Handle up to 10 files
 
-router.get('/getPosts', authMiddleware, getAllPosts);
+router.get('/getPosts', getAllPosts);
 router.put('/:id/like', authMiddleware, like);
 router.get('/:id/comment', authMiddleware, getComment);
 router.put('/:id/save', authMiddleware, savePost);
