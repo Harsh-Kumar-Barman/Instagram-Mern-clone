f444266d5768814fa2bdabaa96562e7aedc944e0
Harsh-Kumar-Barman <harshkumarbarman97@gmail.com>
2025-03-14 13:07:21 +0530

    update protectedRoutjsx

--- frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,13 +2,19 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
+
+const BASE_URL =
+    import.meta.env.MODE === "development"
+        ? import.meta.env.VITE_API_BASE_URL_DEV
+        : import.meta.env.VITE_API_BASE_URL_PROD;
+
 const ProtectedRoute = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
 
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        const res = await axios.get('/api/auth/isLoggedIn');
+        const res = await axios.get(`${BASE_URL}/auth/isLoggedIn`);
         if (res.status === 200) {
           setIsAuthenticated(true);
         }
