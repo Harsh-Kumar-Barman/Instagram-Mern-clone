2bba795276f1653561d2b9a63fa28a720f8e450c
Harsh-Kumar-Barman <harshkumarbarman97@gmail.com>
2025-03-14 16:26:41 +0530

    console login response

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -22,6 +22,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response =await axios.post(`${BASE_URL}/api/auth/login`, { email, password });
+      console.log(response.data)
       const profilePic = response?.data?.user?.profilePicture
       dispatch(addUser({
         fullName: response?.data?.user?.fullName,
