dd5586efea97f5d3856886d35c5945c12bbb9110
Harsh-Kumar-Barman <harshkumarbarman97@gmail.com>
2025-03-14 12:44:12 +0530

    update tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,30 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     darkMode: ["media","class"],
+//     content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//   	extend: {
+//   		borderRadius: {
+//   			lg: 'var(--radius)',
+//   			md: 'calc(var(--radius) - 2px)',
+//   			sm: 'calc(var(--radius) - 4px)'
+//   		},
+//   		colors: {}
+//   	}
+//   },
+//   plugins: [require("tailwindcss-animate")],
+// }
+
+import tailwindAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["media","class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
-}
-
+  plugins: [tailwindAnimate], // ✅ Use ESM import
+};
